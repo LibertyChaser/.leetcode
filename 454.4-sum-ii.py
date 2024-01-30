22f036cc775e806ba265c65b44fcedb1ccfae5d3
Par <liberty_chaser@outlook.com>
2024-01-30 12:22:17 -0600

    finish 454

---
+++ 454.4-sum-ii.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode id=454 lang=python3
+#
+# [454] 4Sum II
+#
+
+# @lc code=start
+class Solution:
+    def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]): # -> int:
+        sum12 = {}
+        result = 0
+        for i in nums1:
+            for j in nums2:
+                sum12[i +j] = sum12.get(i + j, 0) + 1
+        for i in nums3:
+            for j in nums4:
+                if - i - j in sum12:
+                    result += sum12[- i - j]
+        return result
+# @lc code=end
+
