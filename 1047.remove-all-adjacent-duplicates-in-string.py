c76926841c1a79023fd1bbbefb0d4c0ca36369bd
Par <liberty_chaser@outlook.com>
2024-02-14 14:15:05 -0600

    finish 1047 using two pointers method

--- 1047.remove-all-adjacent-duplicates-in-string.py
+++ 1047.remove-all-adjacent-duplicates-in-string.py
@@ -7,12 +7,24 @@
 # @lc code=start
 class Solution:
     def removeDuplicates(self, s: str): # -> str:
-        res = list()
-        for char in s:
-            if res and res[-1] == char:
-                res.pop()
+        # res = list()
+        # for char in s:
+        #     if res and res[-1] == char:
+        #         res.pop()
+        #     else:
+        #         res.append(char)
+        # return "".join(res)
+        left = 0
+        right = 0
+        size = len(s)
+        res = list(s)
+        while (right < size):
+            res[left] = res[right]
+            if left > 0 and res[left] == res[left - 1]:
+                left -= 1
             else:
-                res.append(char)
-        return "".join(res)
+                left += 1
+            right += 1
+        return "".join(res[:left])
 # @lc code=end
 
